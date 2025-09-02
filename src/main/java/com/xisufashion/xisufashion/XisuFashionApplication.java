e8e44d9cf14fc4bf765a68214694b0f923fe364d
phvhieeus <hieuvanpham1211@gmail.com>
2025-09-02 22:53:17 +0700

    first commit

---
+++ src/main/java/com/xisufashion/xisufashion/XisuFashionApplication.java
@@ -0,0 +1,13 @@
+package com.xisufashion.xisufashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XisuFashionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XisuFashionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xisufashion/xisufashion/XisuFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xisufashion.xisufashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XisuFashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
