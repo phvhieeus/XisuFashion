071417c31e01fd7617817f63557b53e16b4a8f3c
phvhieeus <hieuvanpham1211@gmail.com>
2025-09-11 19:27:17 +0700

    Fix Lombok and edit Category

--- src/main/java/com/xisufashion/xisufashion/domain/Category.java
+++ src/main/java/com/xisufashion/xisufashion/domain/Category.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
@@ -14,32 +20,32 @@ public class Category {
 
     private String name;
 
-    // Constructors
-    public Category() {}
-
-    public Category(String name) {
-        this.name = name;
-    }
-
-    public Category(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    // Getters and Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+//    // Constructors
+//    public Category() {}
+//
+//    public Category(String name) {
+//        this.name = name;
+//    }
+//
+//    public Category(Long id, String name) {
+//        this.id = id;
+//        this.name = name;
+//    }
+//
+//    // Getters and Setters
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
